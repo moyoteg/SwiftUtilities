9926114d967403400f6d4d6edd44f705f8b86df2
Moi Gutierrez <moyoteg@gmail.com>
2020-06-05 12:40:36 -0700

    added some comments

--- Tests/UtilitiesTests/UtilitiesTests.swift
+++ Tests/UtilitiesTests/UtilitiesTests.swift
@@ -10,7 +10,8 @@ final class UtilitiesTests: XCTestCase {
         let dataRepresentation = u64.data
         
         let correctByteCount = 8
-        
+
+        // is byte count correct when converting UInt64 to data?
         XCTAssertEqual(dataRepresentation.count, correctByteCount)
     }
     
@@ -19,10 +20,12 @@ final class UtilitiesTests: XCTestCase {
         let data = "test".hexadecimal!
         let data2 = data.hexadecimal.hexadecimal
         
+        // is data the same when converting it from string to hexData and back?
         XCTAssertEqual(data, data2)
     }
     
     static var allTests = [
         ("testByteCount", testByteCount),
+        ("testDataToHexStringConversion", testDataToHexStringConversion),
     ]
 }
