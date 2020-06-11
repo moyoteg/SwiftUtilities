de79a6b0234af400067dc8ebad51a2a13c4f8060
Moi Gutierrez <moyoteg@gmail.com>
2020-06-11 14:43:23 -0700

    added extensions

--- Sources/Utilities/PrivateKeyExtension.swift
+++ Sources/Utilities/PrivateKeyExtension.swift
@@ -32,3 +32,28 @@ public extension SecureEnclave.P256.KeyAgreement.PrivateKey {
         return pemString
     }
 }
+
+public extension P256.KeyAgreement.PrivateKey {
+    
+    var publicKeyPEMFormatString: String {
+        let keyBase64 = self.publicKey.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
+        var pemString = ""
+        let a:[String] = keyBase64.components(separatedBy: "\r\n")
+        pemString.append("-----BEGIN PUBLIC KEY-----\n")
+        a.forEach { (string) in
+            pemString.append(string)
+            pemString.append("\n")
+        }
+        pemString.append("-----END PUBLIC KEY-----\n\n")
+        return pemString
+    }
+    
+    var privateKeyPEMFormatString: String {
+        let keyBase64 = self.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
+        var pemString = ""
+        pemString.append("-----BEGIN PRIVATE KEY-----")
+        pemString.append(keyBase64)
+        pemString.append("-----END PRIVATE KEY-----")
+        return pemString
+    }
+}
