94c8703229cdb3da1a9e754d0027ede8d4e0e7fc
Moi Gutierrez <moyoteg@gmail.com>
2020-06-11 14:44:46 -0700

    added public key pem extension

--- Sources/Utilities/PrivateKeyExtension.swift
+++ Sources/Utilities/PrivateKeyExtension.swift
@@ -57,3 +57,19 @@ public extension P256.KeyAgreement.PrivateKey {
         return pemString
     }
 }
+
+public extension P256.KeyAgreement.PublicKey {
+    
+    var publicKeyPEMFormatString: String {
+        let keyBase64 = self.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
+        var pemString = ""
+        let a:[String] = keyBase64.components(separatedBy: "\r\n")
+        pemString.append("-----BEGIN PUBLIC KEY-----\n")
+        a.forEach { (string) in
+            pemString.append(string)
+            pemString.append("\n")
+        }
+        pemString.append("-----END PUBLIC KEY-----\n\n")
+        return pemString
+    }
+}
