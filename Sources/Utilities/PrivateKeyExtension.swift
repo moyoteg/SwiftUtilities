6d9e39d8ea10ec6c1af578705ed5a4edbba0ea3c
Moi Gutierrez <moyoteg@gmail.com>
2020-06-11 17:23:41 -0700

    updated keys extensions for pem format

--- Sources/Utilities/PrivateKeyExtension.swift
+++ Sources/Utilities/PrivateKeyExtension.swift
@@ -10,66 +10,45 @@ import CryptoKit
 
 public extension SecureEnclave.P256.KeyAgreement.PrivateKey {
     
-    var publicKeyPEMFormatString: String {
-        let keyBase64 = self.publicKey.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
-        var pemString = ""
-        let a:[String] = keyBase64.components(separatedBy: "\r\n")
-        pemString.append("-----BEGIN PUBLIC KEY-----\n")
-        a.forEach { (string) in
-            pemString.append(string)
-            pemString.append("\n")
-        }
-        pemString.append("-----END PUBLIC KEY-----\n\n")
-        return pemString
-    }
-    
-    var privateKeyPEMFormatString: String {
+    var PEMFormatString: String {
         let keyBase64 = self.dataRepresentation.base64EncodedString(options: [.lineLength64Characters])
         var pemString = ""
-        pemString.append("-----BEGIN PRIVATE KEY-----")
+        pemString.append("-----BEGIN PRIVATE KEY-----\n")
         pemString.append(keyBase64)
-        pemString.append("-----END PRIVATE KEY-----")
+        pemString.append("\n")
+        pemString.append("-----END PRIVATE KEY-----\n")
         return pemString
     }
 }
 
 public extension P256.KeyAgreement.PrivateKey {
     
-    var publicKeyPEMFormatString: String {
-        let keyBase64 = self.publicKey.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
+    var PEMFormatString: String {
+        let keyBase64 = self.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
         var pemString = ""
-        let a:[String] = keyBase64.components(separatedBy: "\r\n")
+        let components:[String] = keyBase64.components(separatedBy: "\r\n")
         pemString.append("-----BEGIN PUBLIC KEY-----\n")
-        a.forEach { (string) in
+        components.forEach { (string) in
             pemString.append(string)
             pemString.append("\n")
         }
-        pemString.append("-----END PUBLIC KEY-----\n\n")
-        return pemString
-    }
-    
-    var privateKeyPEMFormatString: String {
-        let keyBase64 = self.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
-        var pemString = ""
-        pemString.append("-----BEGIN PRIVATE KEY-----")
-        pemString.append(keyBase64)
-        pemString.append("-----END PRIVATE KEY-----")
+        pemString.append("-----END PUBLIC KEY-----\n")
         return pemString
     }
 }
 
 public extension P256.KeyAgreement.PublicKey {
     
-    var publicKeyPEMFormatString: String {
+    var PEMFormatString: String {
         let keyBase64 = self.rawRepresentation.base64EncodedString(options: [.lineLength64Characters])
         var pemString = ""
-        let a:[String] = keyBase64.components(separatedBy: "\r\n")
+        let components:[String] = keyBase64.components(separatedBy: "\r\n")
         pemString.append("-----BEGIN PUBLIC KEY-----\n")
-        a.forEach { (string) in
+        components.forEach { (string) in
             pemString.append(string)
             pemString.append("\n")
         }
-        pemString.append("-----END PUBLIC KEY-----\n\n")
+        pemString.append("-----END PUBLIC KEY-----\n")
         return pemString
     }
 }
