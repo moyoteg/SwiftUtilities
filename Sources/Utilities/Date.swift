7440b26cda87b4a084c647091225ccbfbd88c74a
Moi Gutierrez <moyoteg@gmail.com>
2020-07-27 15:39:35 -0700

    added converters

---
+++ Sources/Utilities/Date.swift
@@ -0,0 +1,26 @@
+//
+//  Date.swift
+//  Utilities
+//
+//  Created by Moi Gutierrez on 7/27/20.
+//
+
+import Foundation
+
+public extension Date {
+    var millisecondsSince1970: Int64 {
+        return Int64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+
+    var unsignedMillisecondsSince1970: UInt64 {
+        return UInt64((self.timeIntervalSince1970 * 1000.0).rounded())
+    }
+    
+    init(milliseconds: Int64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
+    
+    init(milliseconds: UInt64) {
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
+    }
+}
