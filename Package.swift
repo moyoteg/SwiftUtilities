35645d976d74c363276cdab77630e3ebdae1d5f3
Moi Gutierrez <moyoteg@gmail.com>
2020-07-30 11:52:46 -0700

    cleanup

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Utilities",
-            dependencies: []),
+            dependencies: [
+        ]),
         .testTarget(
             name: "UtilitiesTests",
             dependencies: ["Utilities"]),
