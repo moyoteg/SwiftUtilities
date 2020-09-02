08d17a2f26fe6b308132a15193eec3976704f1bf
Moi Gutierrez <moyoteg@gmail.com>
2020-09-01 17:43:33 -0700

    added interpreter for string into type for unit state response

--- Sources/Utilities/StringExtensions.swift
+++ Sources/Utilities/StringExtensions.swift
@@ -85,3 +85,13 @@ public extension String {
             .asHexadecimalString
     }
 }
+
+public extension String {
+    var bool: Bool? {
+        switch self.lowercased() {
+        case "true", "t", "yes", "y", "1": return true
+        case "false", "f", "no", "n", "0": return false
+        default:return nil
+        }
+    }
+}
