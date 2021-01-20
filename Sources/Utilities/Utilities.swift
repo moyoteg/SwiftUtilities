833ac1830c243169de7a434933f1f64fe0c71a2f
Moi Gutierrez <moyoteg@gmail.com>
2021-01-19 18:46:52 -0600

    updated debug checker func

--- Sources/Utilities/Utilities.swift
+++ Sources/Utilities/Utilities.swift
@@ -6,7 +6,7 @@ public enum Utilities {
 
         var info = kinfo_proc()
         var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-        var size = MemoryLayout<kinfo_proc>.stride
+        var size = strideofValue(info)
         let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
         assert(junk == 0, "sysctl failed")
         return (info.kp_proc.p_flag & P_TRACED) != 0
