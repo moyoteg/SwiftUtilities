155b648f0bb6354236eed12c2ab7dc2f0b4f7109
Moi Gutierrez <moyoteg@gmail.com>
2021-06-08 00:11:23 -0500

    updated all fonts and recenter location

--- Sources/SwiftUtilities/IntExternsions.swift
+++ Sources/SwiftUtilities/IntExternsions.swift
@@ -141,3 +141,38 @@ public extension CGFloat {
         return CGFloat.random * (max - min) + min
     }
 }
+
+
+public extension Int {
+    /// returns number of digits in Int number
+    var digitCount: Int {
+        get {
+            return numberOfDigits(in: self)
+        }
+    }
+    /// returns number of useful digits in Int number
+    var usefulDigitCount: Int {
+        get {
+            var count = 0
+            for digitOrder in 0..<self.digitCount {
+                /// get each order digit from self
+                let digit = self % (Int(truncating: pow(10, digitOrder + 1) as NSDecimalNumber))
+                    / Int(truncating: pow(10, digitOrder) as NSDecimalNumber)
+                if isUseful(digit) { count += 1 }
+            }
+            return count
+        }
+    }
+    // private recursive method for counting digits
+    private func numberOfDigits(in number: Int) -> Int {
+        if number < 10 && number >= 0 || number > -10 && number < 0 {
+            return 1
+        } else {
+            return 1 + numberOfDigits(in: number/10)
+        }
+    }
+    // returns true if digit is useful in respect to self
+    private func isUseful(_ digit: Int) -> Bool {
+        return (digit != 0) && (self % digit == 0)
+    }
+}
