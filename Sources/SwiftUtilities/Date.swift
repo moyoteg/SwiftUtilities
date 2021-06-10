5366339475973fd31e60ef57bf74444758f3b18c
Moi Gutierrez <moyoteg@gmail.com>
2021-06-10 18:09:06 -0500

    design system, fonts, spacing, location annotation visualization updates

--- Sources/SwiftUtilities/Date.swift
+++ Sources/SwiftUtilities/Date.swift
@@ -40,6 +40,6 @@ public extension Date {
     func timeAgoDisplay() -> String {
         let formatter = RelativeDateTimeFormatter()
         formatter.unitsStyle = .full
-        return formatter.localizedString(for: Date(), relativeTo: self)
+        return formatter.localizedString(for: self, relativeTo: Date())
     }
 }
