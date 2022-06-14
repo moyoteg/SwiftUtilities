32ee87cb3b825956bd91c4193b926cfb853082f2
Moi Gutierrez <moyoteg@gmail.com>
2022-06-14 13:00:57 -0300

    testing exposing without type for module: public var module: Bundle = .module

---
+++ Sources/SwiftUtilities/Extensions/BundleExtension.swift
@@ -0,0 +1,13 @@
+//
+//  BundleExtension.swift
+//  SwiftUtilities
+//
+//  Created by Moi Gutierrez on 6/14/22.
+//
+
+import Foundation
+
+public extension Bundle {
+    
+    static var module: Bundle = .module
+}
