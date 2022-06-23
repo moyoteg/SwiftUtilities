a5c114570bafad8eda40d6cab18c6c3da1de9aa9
Moi Gutierrez <moyoteg@gmail.com>
2022-06-23 00:10:57 -0300

    implementing biometric login [skip ci]

---
+++ Sources/SwiftUtilities/Extensions/LAContextExtension.swift
@@ -0,0 +1,40 @@
+//
+//  LAContextExtension.swift
+//  SwiftUtilities
+//
+//  Created by Moi Gutierrez on 6/22/22.
+//
+
+import Foundation
+import LocalAuthentication
+
+public extension LAContext {
+    
+    enum BiometricType: String {
+        case none
+        case touchID
+        case faceID
+    }
+
+    var biometricType: BiometricType {
+        var error: NSError?
+
+        guard self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return .none
+        }
+
+        if #available(iOS 11.0, *) {
+            switch self.biometryType {
+            case .none:
+                return .none
+            case .touchID:
+                return .touchID
+            case .faceID:
+                return .faceID
+            @unknown default: break
+            }
+        }
+        
+        return  self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touchID : .none
+    }
+}
