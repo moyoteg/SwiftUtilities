961f8f8924f1759f91d69cd473f5b18b59084a8b
Moi Gutierrez <moyoteg@gmail.com>
2022-06-29 13:15:02 -0700

    assets updates, added isAppVersion, cleaned up AppInfo

--- Sources/SwiftUtilities/SwiftUtilities.swift
+++ Sources/SwiftUtilities/SwiftUtilities.swift
@@ -11,4 +11,35 @@ public enum SwiftUtilities {
         assert(junk == 0, "sysctl failed")
         return (info.kp_proc.p_flag & P_TRACED) != 0
     }
+    
+    public enum VersionError: Error {
+        case invalidBundleInfo
+        case invalidResponse
+    }
+    
+    public static func isAppStoreVersion(completion: @escaping (Bool?, Error?) -> Void) throws -> URLSessionDataTask {
+        guard let info = Bundle.main.infoDictionary,
+          let currentVersion = info["CFBundleShortVersionString"] as? String,
+          let identifier = info["CFBundleIdentifier"] as? String else {
+            throw VersionError.invalidBundleInfo
+        }
+        let urlString = "https://itunes.apple.com/gb/lookup?bundleId=\(identifier)"
+        guard let url = URL(string:urlString) else { throw VersionError.invalidBundleInfo }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+          do {
+            if let error = error { throw error }
+            guard let data = data else { throw VersionError.invalidResponse }
+            let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String: Any]
+            guard let result = (json?["results"] as? [Any])?.first as? [String: Any], let appStoreVersion = result["version"] as? String else {
+              throw VersionError.invalidResponse
+            }
+            completion(appStoreVersion == currentVersion, nil)
+          } catch {
+            completion(nil, error)
+          }
+        }
+        task.resume()
+        return task
+    }
+    
 }
