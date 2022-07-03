31121acf86415373e11cf12c14279d371330d741
Moi Gutierrez <moyoteg@gmail.com>
2022-07-03 12:28:23 -0700

    added NetworkReachability

---
+++ Sources/SwiftUtilities/Helpers/Reachability.swift
@@ -0,0 +1,35 @@
+//
+//  Reachability.swift
+//  SwiftUtilities
+//
+//  Created by Moi Gutierrez on 7/3/22.
+//
+
+import Foundation
+
+import Foundation
+import SystemConfiguration
+
+class NetworkReachability: ObservableObject {
+    @Published private(set) var reachable: Bool = false
+    private let reachability = SCNetworkReachabilityCreateWithName(nil, "www.apple.com")
+
+    init() {
+        self.reachable = checkConnection()
+    }
+
+    private func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+        let isReachable = flags.contains(.reachable)
+        let connectionRequired = flags.contains(.connectionRequired)
+        let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        let canConnectWithoutIntervention = canConnectAutomatically && !flags.contains(.interventionRequired)
+        return isReachable && (!connectionRequired || canConnectWithoutIntervention)
+    }
+
+    func checkConnection() -> Bool {
+        var flags = SCNetworkReachabilityFlags()
+        SCNetworkReachabilityGetFlags(reachability!, &flags)
+
+        return isNetworkReachable(with: flags)
+    }
+}
