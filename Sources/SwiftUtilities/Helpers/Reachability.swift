05ab07cefe92a46269bad183a22567ed9346a8d1
Moi Gutierrez <moyoteg@gmail.com>
2022-07-03 12:35:08 -0700

    cleaned up to updateConnectionStatus

--- Sources/SwiftUtilities/Helpers/Reachability.swift
+++ Sources/SwiftUtilities/Helpers/Reachability.swift
@@ -10,24 +10,33 @@ import Foundation
 import Foundation
 import SystemConfiguration
 
-class NetworkReachability: ObservableObject {
+public class NetworkReachability: ObservableObject {
+    
     @Published private(set) var reachable: Bool = false
+    
     private let reachability = SCNetworkReachabilityCreateWithName(nil, "www.apple.com")
 
-    init() {
-        self.reachable = checkConnection()
+    public init() {
+        self.reachable = updateConnectionStatus()
     }
 
     private func isNetworkReachable(with flags: SCNetworkReachabilityFlags) -> Bool {
+        
         let isReachable = flags.contains(.reachable)
+        
         let connectionRequired = flags.contains(.connectionRequired)
+        
         let canConnectAutomatically = flags.contains(.connectionOnDemand) || flags.contains(.connectionOnTraffic)
+        
         let canConnectWithoutIntervention = canConnectAutomatically && !flags.contains(.interventionRequired)
+        
         return isReachable && (!connectionRequired || canConnectWithoutIntervention)
     }
 
-    func checkConnection() -> Bool {
+    public func updateConnectionStatus() -> Bool {
+        
         var flags = SCNetworkReachabilityFlags()
+        
         SCNetworkReachabilityGetFlags(reachability!, &flags)
 
         return isNetworkReachable(with: flags)
